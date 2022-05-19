4ed0614141fe143023c41d2dd4eb5631479f8bef
Maria Antonia Rodrigues Vieira <mariaantoniarvieira@gmail.com>
2022-05-19 11:06:48 +0000

    Initial commit

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private url = 'https://pokeapi.co/api/v2/';
+
+  constructor() { }
+}
